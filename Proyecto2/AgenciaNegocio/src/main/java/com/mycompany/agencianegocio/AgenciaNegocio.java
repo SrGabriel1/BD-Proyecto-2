7c4b814a5b546760e9d6ffc09be11e037df384f7
Gabriel <140923456+SrGabriel1@users.noreply.github.com>
2024-03-20 10:42:24 -0700

    Proyecto 2

---
+++ Proyecto2/AgenciaNegocio/src/main/java/com/mycompany/agencianegocio/AgenciaNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agencianegocio;
+
+/**
+ *
+ * @author yohan
+ */
+public class AgenciaNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
