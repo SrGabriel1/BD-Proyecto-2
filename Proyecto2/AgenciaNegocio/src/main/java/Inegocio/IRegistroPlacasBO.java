7964ce87785b86c089be45c50b154eddec994616
XimenaOA <159595764+XimenaOA@users.noreply.github.com>
2024-04-09 04:57:47 -0700

    documentacion

--- Proyecto2/AgenciaNegocio/src/main/java/Inegocio/IRegistroPlacasBO.java
+++ Proyecto2/AgenciaNegocio/src/main/java/Inegocio/IRegistroPlacasBO.java
@@ -16,27 +16,9 @@ import Excepciones.persistenciaException;
  */
 public interface IRegistroPlacasBO {
 
-    /**
-     * Registra nuevas placas para un automóvil en el sistema.
-     *
-     * @param placa La información de las placas a registrar.
-     * @param auto La información del automóvil al que se asociarán las placas.
-     * @return El número de placas registrado.
-     * @throws PersistenciaException Si ocurre algún error durante la operación
-     * de registro.
-     */
     public String RegistrarPlacasNuevas(PlacaDTO placa, AutomovilDTO auto) throws persistenciaException;
 
-    /**
-     * Registra placas para un automóvil existente en el sistema.
-     *
-     * @param numeroPlaca El número de placa del automóvil al que se registrarán
-     * las nuevas placas.
-     * @param placa La información de las nuevas placas a registrar.
-     * @return El número de placas registrado.
-     * @throws PersistenciaException Si ocurre algún error durante la operación
-     * de registro.
-     */
+  
     public String RegistrarPlacasViejas(String numeroPlaca, PlacaDTO placa) throws persistenciaException;
 
 }
